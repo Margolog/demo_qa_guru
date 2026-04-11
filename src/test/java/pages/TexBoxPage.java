2d4a5b43246703309ee2fb4768fd3cf1466df0e5
Margarita Logunova <logunovar@gmail.com>
2026-04-11 18:34:30 +0300

    add TexBoxPage

---
+++ src/test/java/pages/TexBoxPage.java
@@ -0,0 +1,56 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class TexBoxPage {
+
+    //Elements
+    private SelenideElement userNameInput = $("#userName");
+    private SelenideElement userEmailInput = $("#userEmail");
+    private SelenideElement currentAddressInput = $("#currentAddress");
+    private SelenideElement permanentAddressInput = $("#permanentAddress");
+    private SelenideElement submitButton = $("#submit");
+    private SelenideElement outputResults = $("#output");
+
+    //Actions
+    public TexBoxPage openPage() {
+        open("/text-box");
+        return this;
+    }
+
+    public TexBoxPage typeUserName(String value) {
+        userNameInput.setValue(value);
+        return this;
+    }
+
+    public TexBoxPage typeUserEmail(String value) {
+        userEmailInput.setValue(value);
+        return this;
+    }
+
+    public TexBoxPage typeCurrentAddress(String value) {
+        currentAddressInput.setValue(value);
+        return this;
+    }
+
+    public TexBoxPage typePermanentAddress(String value) {
+        permanentAddressInput.setValue(value);
+        return this;
+    }
+
+    public TexBoxPage clickSubmitForm() {
+        submitButton.click();
+        return this;
+    }
+
+    public TexBoxPage checkField(String key, String value) {
+        outputResults.$(byId(key)).shouldHave(text(value));
+        return this;
+    }
+}
