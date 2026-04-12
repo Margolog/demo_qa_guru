d3ca431737be00457e88dd9f58c3e9e6488e30b4
Margarita Logunova <logunovar@gmail.com>
2026-04-12 19:50:38 +0300

    fix reg form

--- src/test/java/pages/components/RegistrationCheckComponent.java
+++ src/test/java/pages/components/RegistrationCheckComponent.java
@@ -1,17 +1,16 @@
 package pages.components;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
-
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class RegistrationCheckComponent {
-    private SelenideElement outputResults = $(".table-responsive tr");
 
     public RegistrationCheckComponent checkFormResults(String key, String value) {
-        outputResults
-                .find(String.valueOf(Condition.text(key)))
-                .find(String.valueOf(Condition.text(value)));
+        $$("tbody tr")
+                .findBy(text(key))
+                .$$("td")
+                .get(1)
+                .shouldHave(text(value));
         return this;
     }
 }
