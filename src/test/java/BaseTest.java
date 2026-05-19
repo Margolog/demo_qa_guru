368d5308638d0354c70d1654a28616b2c05b842f
Margarita Logunova <logunovar@gmail.com>
2026-05-19 22:02:29 +0300

    fix TestData

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -36,5 +37,9 @@ public class BaseTest {
     @AfterEach
     void afterEach() {
         closeWebDriver();
+        Attach.screenshotAs("Last screenshotA");
+        Attach.pageSource();
+        Attach.addVideo();
+        Attach.browserConsoleLogs();
     }
 }
